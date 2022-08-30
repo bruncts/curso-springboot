5788d1e1a1c8cebe322601bd428dbefdb610769c
Bruno Silva <bruno.cassiobdc@gmail.com>
2022-08-30 16:36:31 -0300

    Exception handling - delete

--- src/main/java/com/bruncts/course/services/UserCService.java
+++ src/main/java/com/bruncts/course/services/UserCService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.bruncts.course.entities.UserC;
 import com.bruncts.course.repositories.UserCRepository;
+import com.bruncts.course.services.exceptions.DatabaseException;
 import com.bruncts.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserCService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public UserC update(Long id, UserC obj) {
